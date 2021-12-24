060643a8e18795dc7d91883972f125397e4a1d16
Yan Hui <me@yanh.org>
2021-12-24 21:40:31 +0100

    clean

--- scripts/cluster_ava_alignments.py
+++ scripts/cluster_ava_alignments.py
@@ -224,10 +224,6 @@ def main(args):
     if args.max_recursion > 0:
         sys.setrecursionlimit(args.max_recursion)
 
-    #cols = ['qname', 'qlen', 'qstart', 'qend', 'strand', 'tname', \
-    #        'tlen', 'tstart', 'tend', 'matches', 'alnlen', 'mapqv', \
-    #        'tp', 'cm', 's1', 'dv', 'rl']
-    #df = pd.read_csv(args.paf, sep='\t', header=None, usecols=list(range(17)), names=cols)
     cols = ['qname', 'qlen', 'tname', 'tlen', 'alnlen', 'dv']
     dtypes = {0:"category", 1:"int16", 5:"category", 6:"int16", 10:"int16", 15:"category"}
     df = pd.read_csv(args.paf, sep='\t', header=None, usecols=[0,1,5,6,10,15], names=cols, dtype=dtypes)
